0d33d5c82880b618d5063b830695e780a984db19
ZaharScript <zahratulnazirah@gmail.com>
2024-11-13 01:40:34 +0800

    adding layout templete.

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -45,7 +45,7 @@
                     <div class="card-body pt-3">
                         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="#">
+                                <a class="nav-link text-dark" href="/index">
                                     <span>Home</span></a>
                             </li>
                             <li class="nav-item">
@@ -57,7 +57,7 @@
                                     <span>Feed</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="/term">
                                     <span>Terms</span></a>
                             </li>
                             <li class="nav-item">
@@ -91,60 +91,61 @@
                 </div>
                 <hr>
                 {{-- page contents goes here --}}
-                <div class="col-3">
-                    <div class="card">
-                        <div class="card-header pb-0 border-0">
-                            <h5 class="">Search</h5>
-                        </div>
-                        <div class="card-body">
-                            <input placeholder="...
-                        " class="form-control w-100" type="text"
-                                id="search">
-                            <button class="btn btn-dark mt-2"> Search</button>
-                        </div>
+                @yield('content')
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header pb-0 border-0">
+                        <h5 class="">Search</h5>
                     </div>
-                    <div class="card mt-3">
-                        <div class="card-header pb-0 border-0">
-                            <h5 class="">Who to follow</h5>
+                    <div class="card-body">
+                        <input placeholder="...
+                        " class="form-control w-100" type="text"
+                            id="search">
+                        <button class="btn btn-dark mt-2"> Search</button>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <div class="card-header pb-0 border-0">
+                        <h5 class="">Who to follow</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="hstack gap-2 mb-3">
+                            <div class="avatar">
+                                <a href="#!"><img class="avatar-img rounded-circle"
+                                        src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario" alt=""></a>
+                            </div>
+                            <div class="overflow-hidden">
+                                <a class="h6 mb-0" href="#!">Mario Brother</a>
+                                <p class="mb-0 small text-truncate">@Mario</p>
+                            </div>
+                            <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i
+                                    class="fa-solid fa-plus"> </i></a>
                         </div>
-                        <div class="card-body">
-                            <div class="hstack gap-2 mb-3">
-                                <div class="avatar">
-                                    <a href="#!"><img class="avatar-img rounded-circle"
-                                            src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario"
-                                            alt=""></a>
-                                </div>
-                                <div class="overflow-hidden">
-                                    <a class="h6 mb-0" href="#!">Mario Brother</a>
-                                    <p class="mb-0 small text-truncate">@Mario</p>
-                                </div>
-                                <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i
-                                        class="fa-solid fa-plus"> </i></a>
+                        <div class="hstack gap-2 mb-3">
+                            <div class="avatar">
+                                <a href="#!"><img class="avatar-img rounded-circle"
+                                        src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario"
+                                        alt=""></a>
                             </div>
-                            <div class="hstack gap-2 mb-3">
-                                <div class="avatar">
-                                    <a href="#!"><img class="avatar-img rounded-circle"
-                                            src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario"
-                                            alt=""></a>
-                                </div>
-                                <div class="overflow-hidden">
-                                    <a class="h6 mb-0" href="#!">Mario Brother</a>
-                                    <p class="mb-0 small text-truncate">@Mario</p>
-                                </div>
-                                <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i
-                                        class="fa-solid fa-plus"> </i></a>
+                            <div class="overflow-hidden">
+                                <a class="h6 mb-0" href="#!">Mario Brother</a>
+                                <p class="mb-0 small text-truncate">@Mario</p>
                             </div>
-                            <div class="d-grid mt-3">
-                                <a class="btn btn-sm btn-primary-soft" href="#!">Show More</a>
-                            </div>
+                            <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i
+                                    class="fa-solid fa-plus"> </i></a>
+                        </div>
+                        <div class="d-grid mt-3">
+                            <a class="btn btn-sm btn-primary-soft" href="#!">Show More</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-        </script>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/twitter/term.blade.php
+++ resources/views/twitter/term.blade.php
@@ -1,4 +1,6 @@
-<div>
+@extends('layouts.layouts')
+
+@section('content')
     <h1>Term</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam assumenda minus temporibus magnam dignissimos
         esse ipsa quisquam impedit velit earum. Perspiciatis et harum eaque debitis rerum quidem sit modi assumenda
@@ -8,4 +10,4 @@
         nesciunt! Quidem quas minus quis nostrum, modi magnam id quod. Quia sequi, explicabo dolor tempore nam
         voluptatum similique ratione rerum pariatur eligendi facilis quisquam aperiam praesentium? Ducimus, dolorum.
         Obcaecati!</p>
-</div>
+@endsection
